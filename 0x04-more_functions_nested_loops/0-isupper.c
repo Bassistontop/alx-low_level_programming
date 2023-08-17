064cb534018897ba5be8be48688fd88ca13116b5
Bassistontop <armaharyee1@gmail.com>
2023-08-17 11:56:39 -0700

    detects upper case alphabets

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
  * _isupper - detect upper case alphabets
  *
  * @c: input character
  *
- * Return: (1) for true (0) for false
+ * Return: (1) for true (0) false
+ *
  */
-
-int _isupper(int c);
+int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {
