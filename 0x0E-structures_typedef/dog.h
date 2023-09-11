110b2ddd389d891fb0b37613c5f70d614000d69c
Bassistontop <armaharyee1@gmail.com>
2023-09-11 10:39:28 -0700

    a struct that prints dog info

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct for dog info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner info of the dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
