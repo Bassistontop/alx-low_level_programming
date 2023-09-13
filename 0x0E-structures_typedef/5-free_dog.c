1e5a0600e2efafbd7b81b6ec2325d5943b5e416b
Bassistontop <armaharyee1@gmail.com>
2023-09-13 08:26:28 -0700

    freeing dog allocated memory

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 /**
- * free_dog - a function that frees a dog
- * @d: struct dog to free
+ * free_dog - Frees dogs.
+ * @d: The dog to be freed.
  */
-
 void free_dog(dog_t *d)
 {
-	if (d)
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
 }
