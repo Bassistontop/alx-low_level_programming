50112735fb7e7a1b1037a04dfc2a00bd0c590b5d
Bassistontop <armaharyee1@gmail.com>
2023-09-14 12:48:01 -0700

    print all numbers correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,11 +1,11 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
-#include <stdio.h>
+
 
 /**
  * print_numbers - a function that prints numbers followed by a nw line
  *
- * @seperator: string to be printed between the numbers
+ * @separator: string to be printed between the numbers
  * @n: the number of intergers passed
  *
  * @...: variable arguments to parameter
@@ -13,20 +13,44 @@
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
+{
 	va_list nums;
 	unsigned int index;
 
 	va_start(nums, n);
 
 	for (index = 0; index < n; index++)
-{
-	printf("%d", va_arg(nums, int));
+	{
+		int num = va_arg(nums, int);
 
-	if (index != (n - 1) && separator != NULL)
-		printf("%s", separator);
-}
+		int temp = num;
+		int digit_count = 0;
 
-printf("\n");
+		do {
+			temp /= 10;
+			digit_count++;
+		} while (temp != 0);
 
-va_end(nums);
+		temp = num;
+		for (int j = digit_count - 1; j >= 0; j--)
+		{
+			char digit = (temp / (int)pow(10, j)) + '0';
+
+			putchar(digit);
+			temp %= (int)pow(10, j);
+		}
+
+		if (index != (n - 1) && separator != NULL)
+		{
+			while (*separator != '\0')
+			{
+				putchar(*separator);
+				separator++;
+			}
+		}
+	}
+
+	va_end(nums);
+
+	putchar('\n');
 }
